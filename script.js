8b9851b3e71971e9730b0d64ac8672d1e4afe1a9
spytni1212 <avdeenkoartem1212@gmail.com>
2020-03-11 01:42:49 +0300

    feat: add script for geader

---
+++ script.js
@@ -0,0 +1,8 @@
+const MENU = document.getElementById('menu');
+
+
+
+MENU.addEventListener('click', (event) => {
+    MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+});
