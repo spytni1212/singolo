996929abb2b4bf9d38ef83f66ee4b4b12e1e39b4
spytni1212 <avdeenkoartem1212@gmail.com>
2020-03-11 19:12:33 +0300

    feat: add script for section portfolio(tab switching)

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const MENU = document.getElementById('menu');
+const WORKS = document.getElementById('works');
+const imagesBlock = document.getElementById('works');
+const portfolioNavButtons = document.getElementById('portfolio-navigation');
+
 
 MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
@@ -6,10 +10,19 @@ MENU.addEventListener('click', (event) => {
 });
 
 
-
-const WORKS = document.getElementById('works');
-
 WORKS.addEventListener('click', (event) => {
     WORKS.querySelectorAll('img').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
+
+
+portfolioNavButtons.addEventListener('click', (event) => {
+    portfolioNavButtons.querySelectorAll('.portfolio__button').forEach(item => item.classList.remove('active'));
+    event.target.classList.add('active');
+
+    let firstChild = imagesBlock.firstChild
+    imagesBlock.removeChild(imagesBlock.firstChild);
+    imagesBlock.appendChild(firstChild)
+});
+
+
