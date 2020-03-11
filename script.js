a849e6a1ae42f297de1758fcabee155a2f69ca16
spytni1212 <avdeenkoartem1212@gmail.com>
2020-03-11 15:25:18 +0300

    feat: add script fo section portfolio(interaction with img)

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const MENU = document.getElementById('menu');
 
-
-
 MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+});
+
+
+
+const WORKS = document.getElementById('works');
+
+WORKS.addEventListener('click', (event) => {
+    WORKS.querySelectorAll('img').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
