49cda2fe384a3a05318697db1f4f4719dcec004f
spytni1212 <avdeenkoartem1212@gmail.com>
2020-03-14 15:15:01 +0300

    feat: add script for form submission

--- script.js
+++ script.js
@@ -20,9 +20,36 @@ portfolioNavButtons.addEventListener('click', (event) => {
     portfolioNavButtons.querySelectorAll('.portfolio__button').forEach(item => item.classList.remove('active'));
     event.target.classList.add('active');
 
-    let firstChild = imagesBlock.firstChild
+    let firstChild = imagesBlock.firstChild;
     imagesBlock.removeChild(imagesBlock.firstChild);
     imagesBlock.appendChild(firstChild)
+});
+
+
+const closeButton = document.getElementById('close-button');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let subject = document.getElementById('subject').value.toString();
+    let describe = document.getElementById('describe').value.toString();
+    if (subject === "") {
+        document.getElementById('text-result').innerHTML = "Без Темы";  
+    } else {
+        document.getElementById('result-subject').innerText = subject;    
+    }
+    
+    if (describe === "") {
+        document.getElementById('text-describe').innerHTML = "Без описания";
+    } else {
+        document.getElementById('result-describe').innerText = describe;
+    }
+    document.getElementById('message-block').classList.remove('hidden');
+    form.reset();
+});
+
+closeButton.addEventListener('click', () => {
+    document.getElementById('message-block').classList.add('hidden');
 });
 
 
